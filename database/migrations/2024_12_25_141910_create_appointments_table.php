ce011eab6361f98c7edbf67ad0ab80a99ae1bd4f
root <jtanjoshua@gmail.com>
2024-12-26 21:42:33 +0800

    appointments

---
+++ database/migrations/2024_12_25_141910_create_appointments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            // tenant relationships
+            $table->foreignId('tenant_id')->constrained()->cascadeOnDelete();
+
+            // appointment data
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+
+            // patient data (either linked to a patient or just a name)
+            $table->foreignId('patient_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->string('patient_name')->nullable();
+
+            $table->string('remarks')->nullable();
+            $table->string('notes')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
