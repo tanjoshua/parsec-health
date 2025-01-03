8764d4f2178a76dda6bf5567a9e124557cf3e50d
root <jtanjoshua@gmail.com>
2025-01-03 23:14:22 +0800

    save

--- resources/js/Layouts/Authenticated.svelte
+++ resources/js/Layouts/Authenticated.svelte
@@ -1,4 +1,6 @@
 <script>
+    import * as Sidebar from "@/components/ui/sidebar";
+    import AppSidebar from "@/components/shared/app-sidebar.svelte";
     import BreezeApplicationLogo from "@/Components/ApplicationLogo.svelte";
     import BreezeDropdown from "@/Components/Dropdown.svelte";
     import BreezeDropdownLink from "@/Components/DropdownLink.svelte";
@@ -8,24 +10,27 @@
     export let user = $page.props.auth.user;
 </script>
 
-<div>
-    <div class="min-h-screen">
-        <nav class="bg-white border-b border-gray-100">
-            <!-- Primary Navigation Menu -->
-            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div class="flex justify-between h-16">
-                    <div class="flex">
-                        <!-- Logo -->
-                        <div class="shrink-0 flex items-center">
-                            <Link href="/dashboard">
-                                <BreezeApplicationLogo
-                                    class="block h-9 w-auto"
-                                />
-                            </Link>
-                        </div>
+<Sidebar.Provider>
+    <AppSidebar />
+    <Sidebar.Inset>
+        <div>
+            <div class="min-h-screen">
+                <nav class="bg-white border-b border-gray-100">
+                    <!-- Primary Navigation Menu -->
+                    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div class="flex justify-between h-16">
+                            <div class="flex">
+                                <!-- Logo -->
+                                <div class="shrink-0 flex items-center">
+                                    <Link href="/dashboard">
+                                        <BreezeApplicationLogo
+                                            class="block h-9 w-auto"
+                                        />
+                                    </Link>
+                                </div>
 
-                        <!-- Navigation Links -->
-                        <!-- <div
+                                <!-- Navigation Links -->
+                                <!-- <div
                             class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex"
                         >
                             <BreezeNavLink
@@ -47,118 +52,113 @@
                                 Links
                             </BreezeNavLink>
                         </div> -->
-                    </div>
+                            </div>
 
-                    <div class="flex items-center">
-                        <!-- Settings Dropdown -->
-                        <div class="ml-3 relative">
-                            <BreezeDropdown
-                                class="w-48 origin-top-right right-0"
-                            >
-                                <span
-                                    class="inline-flex rounded-md"
-                                    slot="trigger"
-                                >
-                                    <button
-                                        type="button"
-                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                            <div class="flex items-center">
+                                <!-- Settings Dropdown -->
+                                <div class="ml-3 relative">
+                                    <BreezeDropdown
+                                        class="w-48 origin-top-right right-0"
                                     >
-                                        {user.name}
-
-                                        <svg
-                                            class="ml-2 -mr-0.5 h-4 w-4"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20"
-                                            fill="currentColor"
+                                        <span
+                                            class="inline-flex rounded-md"
+                                            slot="trigger"
                                         >
-                                            <path
-                                                fill-rule="evenodd"
-                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                clip-rule="evenodd"
-                                            />
-                                        </svg>
-                                    </button>
-                                </span>
-                                <div slot="content">
-                                    <BreezeDropdownLink
-                                        href="/logout"
-                                        method="post"
-                                        as="button"
-                                        type="button"
-                                    >
-                                        Log Out
-                                    </BreezeDropdownLink>
+                                            <button
+                                                type="button"
+                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                            >
+                                                {user.name}
+
+                                                <svg
+                                                    class="ml-2 -mr-0.5 h-4 w-4"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 20 20"
+                                                    fill="currentColor"
+                                                >
+                                                    <path
+                                                        fill-rule="evenodd"
+                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                        clip-rule="evenodd"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </span>
+                                        <div slot="content">
+                                            <BreezeDropdownLink
+                                                href="/logout"
+                                                method="post"
+                                                as="button"
+                                                type="button"
+                                            >
+                                                Log Out
+                                            </BreezeDropdownLink>
+                                        </div>
+                                    </BreezeDropdown>
                                 </div>
-                            </BreezeDropdown>
+                            </div>
                         </div>
                     </div>
-                </div>
+
+                    <!-- Responsive Navigation Menu -->
+                    <div
+                        class:block={showingNavigationDropdown}
+                        class:hidden={!showingNavigationDropdown}
+                        class="sm:hidden"
+                    >
+                        <div class="pt-2 pb-3 space-y-1">
+                            <BreezeResponsiveNavLink
+                                href="/dashboard"
+                                active={$page.component === "Dashboard"}
+                            >
+                                Dashboard
+                            </BreezeResponsiveNavLink>
+                            <BreezeResponsiveNavLink
+                                href="/about"
+                                active={$page.component === "About"}
+                            >
+                                About
+                            </BreezeResponsiveNavLink>
+                            <BreezeResponsiveNavLink
+                                href="/links"
+                                active={$page.component === "Links/Index"}
+                            >
+                                Links
+                            </BreezeResponsiveNavLink>
+                        </div>
+
+                        <!-- Responsive Settings Options -->
+                        <div class="pt-4 pb-1 border-t border-gray-200">
+                            <div class="px-4">
+                                <div
+                                    class="font-medium text-base text-gray-800"
+                                >
+                                    {user.name}
+                                </div>
+                                <div class="font-medium text-sm text-gray-500">
+                                    {user.email}
+                                </div>
+                            </div>
+
+                            <div class="mt-3 space-y-1">
+                                <BreezeResponsiveNavLink
+                                    href="/logout"
+                                    method="post"
+                                    as="button"
+                                >
+                                    Log Out
+                                </BreezeResponsiveNavLink>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+
+                <!-- Page Content -->
+                <main>
+                    <Sidebar.Trigger />
+                    <slot />
+                </main>
             </div>
-
-            <!-- Responsive Navigation Menu -->
-            <div
-                class:block={showingNavigationDropdown}
-                class:hidden={!showingNavigationDropdown}
-                class="sm:hidden"
-            >
-                <div class="pt-2 pb-3 space-y-1">
-                    <BreezeResponsiveNavLink
-                        href="/dashboard"
-                        active={$page.component === "Dashboard"}
-                    >
-                        Dashboard
-                    </BreezeResponsiveNavLink>
-                    <BreezeResponsiveNavLink
-                        href="/about"
-                        active={$page.component === "About"}
-                    >
-                        About
-                    </BreezeResponsiveNavLink>
-                    <BreezeResponsiveNavLink
-                        href="/links"
-                        active={$page.component === "Links/Index"}
-                    >
-                        Links
-                    </BreezeResponsiveNavLink>
-                </div>
-
-                <!-- Responsive Settings Options -->
-                <div class="pt-4 pb-1 border-t border-gray-200">
-                    <div class="px-4">
-                        <div class="font-medium text-base text-gray-800">
-                            {user.name}
-                        </div>
-                        <div class="font-medium text-sm text-gray-500">
-                            {user.email}
-                        </div>
-                    </div>
-
-                    <div class="mt-3 space-y-1">
-                        <BreezeResponsiveNavLink
-                            href="/logout"
-                            method="post"
-                            as="button"
-                        >
-                            Log Out
-                        </BreezeResponsiveNavLink>
-                    </div>
-                </div>
-            </div>
-        </nav>
-
-        <!-- Page Heading -->
-        {#if $$slots.header}
-            <!-- content here -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <slot name="header" />
-                </div>
-            </header>
-        {/if}
-
-        <!-- Page Content -->
-        <main>
-            <slot />
-        </main>
-    </div>
-</div>
+        </div>
+    </Sidebar.Inset>
+</Sidebar.Provider>
