bdbd84924e86c56c4f3c8419d2208cf6d8cd3d39
root <jtanjoshua@gmail.com>
2025-01-04 21:06:55 +0800

    update navbar

--- resources/js/Layouts/Authenticated.svelte
+++ resources/js/Layouts/Authenticated.svelte
@@ -6,6 +6,8 @@
     import BreezeDropdownLink from "@/Components/DropdownLink.svelte";
     import BreezeResponsiveNavLink from "@/Components/ResponsiveNavLink.svelte";
     import { page, Link } from "@inertiajs/svelte";
+    import { Separator } from "@/components/ui/separator";
+    import * as Breadcrumb from "@/components/ui/breadcrumb";
     let showingNavigationDropdown = false;
     export let user = $page.props.auth.user;
 </script>
@@ -13,152 +15,84 @@
 <Sidebar.Provider>
     <AppSidebar />
     <Sidebar.Inset>
-        <div>
-            <div class="min-h-screen">
-                <nav class="bg-white border-b border-gray-100">
-                    <!-- Primary Navigation Menu -->
-                    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                        <div class="flex justify-between h-16">
-                            <div class="flex">
-                                <!-- Logo -->
-                                <div class="shrink-0 flex items-center">
-                                    <Link href="/dashboard">
-                                        <BreezeApplicationLogo
-                                            class="block h-9 w-auto"
-                                        />
-                                    </Link>
-                                </div>
-
-                                <!-- Navigation Links -->
-                                <!-- <div
-                            class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex"
-                        >
-                            <BreezeNavLink
-                                href="/dashboard"
-                                active={$page.component === "Dashboard"}
-                            >
-                                Dashboard
-                            </BreezeNavLink>
-                            <BreezeNavLink
-                                href="/about"
-                                active={$page.component === "About"}
-                            >
-                                About
-                            </BreezeNavLink>
-                            <BreezeNavLink
-                                href="/links"
-                                active={$page.component === "Links/Index"}
-                            >
-                                Links
-                            </BreezeNavLink>
-                        </div> -->
-                            </div>
-
-                            <div class="flex items-center">
-                                <!-- Settings Dropdown -->
-                                <div class="ml-3 relative">
-                                    <BreezeDropdown
-                                        class="w-48 origin-top-right right-0"
-                                    >
-                                        <span
-                                            class="inline-flex rounded-md"
-                                            slot="trigger"
+        <div class="min-h-screen">
+            <nav class="border-b">
+                <!-- Primary Navigation Menu -->
+                <div class="px-4">
+                    <div class="flex justify-between h-16">
+                        <div class="flex items-center gap-4">
+                            <Sidebar.Trigger />
+                            <!-- <Separator orientation="vertical" class=" " />
+                            <Breadcrumb.Root>
+                                <Breadcrumb.List>
+                                    <Breadcrumb.Item class="hidden md:block">
+                                        <Breadcrumb.Link href="#"
+                                            >Building Your Application</Breadcrumb.Link
                                         >
-                                            <button
-                                                type="button"
-                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                            >
-                                                {user.name}
+                                    </Breadcrumb.Item>
+                                    <Breadcrumb.Separator
+                                        class="hidden md:block"
+                                    />
+                                    <Breadcrumb.Item>
+                                        <Breadcrumb.Page
+                                            >Data Fetching</Breadcrumb.Page
+                                        >
+                                    </Breadcrumb.Item>
+                                </Breadcrumb.List>
+                            </Breadcrumb.Root> -->
+                        </div>
 
-                                                <svg
-                                                    class="ml-2 -mr-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fill-rule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clip-rule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                        <div slot="content">
-                                            <BreezeDropdownLink
-                                                href="/logout"
-                                                method="post"
-                                                as="button"
-                                                type="button"
+                        <div class="flex items-center">
+                            <!-- Settings Dropdown -->
+                            <div class="ml-3 relative">
+                                <BreezeDropdown
+                                    class="w-48 origin-top-right right-0"
+                                >
+                                    <span
+                                        class="inline-flex rounded-md"
+                                        slot="trigger"
+                                    >
+                                        <button
+                                            type="button"
+                                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                        >
+                                            {user.name}
+
+                                            <svg
+                                                class="ml-2 -mr-0.5 h-4 w-4"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20"
+                                                fill="currentColor"
                                             >
-                                                Log Out
-                                            </BreezeDropdownLink>
-                                        </div>
-                                    </BreezeDropdown>
-                                </div>
+                                                <path
+                                                    fill-rule="evenodd"
+                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                    clip-rule="evenodd"
+                                                />
+                                            </svg>
+                                        </button>
+                                    </span>
+                                    <div slot="content">
+                                        <BreezeDropdownLink
+                                            href="/logout"
+                                            method="post"
+                                            as="button"
+                                            type="button"
+                                        >
+                                            Log Out
+                                        </BreezeDropdownLink>
+                                    </div>
+                                </BreezeDropdown>
                             </div>
                         </div>
                     </div>
+                </div>
+            </nav>
 
-                    <!-- Responsive Navigation Menu -->
-                    <div
-                        class:block={showingNavigationDropdown}
-                        class:hidden={!showingNavigationDropdown}
-                        class="sm:hidden"
-                    >
-                        <div class="pt-2 pb-3 space-y-1">
-                            <BreezeResponsiveNavLink
-                                href="/dashboard"
-                                active={$page.component === "Dashboard"}
-                            >
-                                Dashboard
-                            </BreezeResponsiveNavLink>
-                            <BreezeResponsiveNavLink
-                                href="/about"
-                                active={$page.component === "About"}
-                            >
-                                About
-                            </BreezeResponsiveNavLink>
-                            <BreezeResponsiveNavLink
-                                href="/links"
-                                active={$page.component === "Links/Index"}
-                            >
-                                Links
-                            </BreezeResponsiveNavLink>
-                        </div>
-
-                        <!-- Responsive Settings Options -->
-                        <div class="pt-4 pb-1 border-t border-gray-200">
-                            <div class="px-4">
-                                <div
-                                    class="font-medium text-base text-gray-800"
-                                >
-                                    {user.name}
-                                </div>
-                                <div class="font-medium text-sm text-gray-500">
-                                    {user.email}
-                                </div>
-                            </div>
-
-                            <div class="mt-3 space-y-1">
-                                <BreezeResponsiveNavLink
-                                    href="/logout"
-                                    method="post"
-                                    as="button"
-                                >
-                                    Log Out
-                                </BreezeResponsiveNavLink>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-
-                <!-- Page Content -->
-                <main>
-                    <Sidebar.Trigger />
-                    <slot />
-                </main>
-            </div>
+            <!-- Page Content -->
+            <main>
+                <slot />
+            </main>
         </div>
     </Sidebar.Inset>
 </Sidebar.Provider>
