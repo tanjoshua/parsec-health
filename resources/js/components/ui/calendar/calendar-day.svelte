6ce25811d68f486ac95befc4aeda34654aa078c8
root <jtanjoshua@gmail.com>
2025-01-28 22:37:18 +0800

    highlight current day clearer

--- resources/js/components/ui/calendar/calendar-day.svelte
+++ resources/js/components/ui/calendar/calendar-day.svelte
@@ -15,7 +15,7 @@
 		buttonVariants({ variant: "ghost" }),
 		"size-8 p-0 font-normal",
 		// Today
-		"[&[data-today]:not([data-selected])]:bg-accent [&[data-today]:not([data-selected])]:text-accent-foreground",
+		"[&[data-today]:not([data-selected])]:bg-accent [&[data-today]:not([data-selected])]:text-accent-foreground [&[data-today]:not([data-selected])]:border [&[data-today]:not([data-selected])]:border-gray-300",
 		// Selected
 		"data-[selected]:bg-primary data-[selected]:text-primary-foreground data-[selected]:hover:bg-primary data-[selected]:hover:text-primary-foreground data-[selected]:focus:bg-primary data-[selected]:focus:text-primary-foreground data-[selected]:opacity-100",
 		// Disabled
@@ -24,7 +24,7 @@
 		"data-[unavailable]:text-destructive-foreground data-[unavailable]:line-through",
 		// Outside months
 		"data-[outside-month]:text-muted-foreground [&[data-outside-month][data-selected]]:bg-accent/50 [&[data-outside-month][data-selected]]:text-muted-foreground data-[outside-month]:pointer-events-none data-[outside-month]:opacity-50 [&[data-outside-month][data-selected]]:opacity-30",
-		className
+		className,
 	)}
 	bind:ref
 	{...restProps}
